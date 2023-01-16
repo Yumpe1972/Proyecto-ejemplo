d35203e742c30ca72b8113bbded9e36a2a0b9a01
Yumpe1972 <juanpedro.sanchezmartinez@gmail.com>
2023-01-16 18:11:30 +0100

    Eliminar target de git y comienzo resolución Ejercicio01

---
+++ src/main/java/com/unisys/ejercicios/Ejercicio01.java
@@ -0,0 +1,18 @@
+package com.unisys.ejercicios;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        short marianico = 12;
+        long gasol = 4000;
+        boolean independencia=false;
+        double presupuesto=12200;
+        float longitud=16.34f;
+//    var suma = gasol+marianico;
+        System.out.println();
+
+    }
+
+
+}
