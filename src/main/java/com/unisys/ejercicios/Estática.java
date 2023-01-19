cc04fa1455c0700b3fff5bb91543f226883127f1
Yumpe1972 <juanpedro.sanchezmartinez@gmail.com>
2023-01-19 16:20:37 +0100

    Ejercicios de Estáticos

---
+++ src/main/java/com/unisys/ejercicios/Estática.java
@@ -0,0 +1,8 @@
+package com.unisys.ejercicios;
+
+public class Estática {
+    static float pi=3.14f;
+    static String berrinche(){
+        return "Quejido";
+    }
+}
---
+++ src/main/java/com/unisys/ejercicios/Programa.java
@@ -0,0 +1,8 @@
+package com.unisys.ejercicios;
+
+public class Programa {
+    public static void main(String[] args) {
+        System.out.println(Estática.pi);
+        System.out.println(Estática.berrinche());
+    }
+}
